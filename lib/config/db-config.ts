9a031c2ffdf22bced93faf57a9fe64bb6d997003
Khash <khashayar1924@gmail.com>
2024-04-13 17:30:44 -0400

    fix database connection in deployed version

--- lib/config/db-config.ts
+++ lib/config/db-config.ts
@@ -1,14 +1,32 @@
 import mongoose from "mongoose";
 
-export async function connect() {
-  try {
-    mongoose.connect(process.env.MONGO_URI!);
+const MONGODB_URI = process.env.MONGO_URI;
 
-    mongoose.connection.on("error", (err) => {
-      console.log("Unable to connect to mongo" + err);
-      process.exit();
-    });
-  } catch (error: any) {
-    console.log(error);
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+const { MONGO_URI } = process.env;
+
+if (!MONGO_URI) throw new Error("MONGODB_DB not defined");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(`${MONGO_URI}}`)
+      .then((mongoose) => mongoose);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
